9436b976d9f070f87d14853ab36fc77197bd26d7
unknown <pawel.r.niziolek@gmail.com>
2022-11-01 21:49:53 +0100

    creating code for footer - mobile and desktop

--- app/public/wp-content/themes/skoraodnowatheme/footer.php
+++ app/public/wp-content/themes/skoraodnowatheme/footer.php
@@ -1,5 +1,59 @@
-<h2>footer</h2>
+<footer class="wrap-footer">
+  <div class="wrap-footer__wrap-section">
+    <section class="wrap-footer__section footer-visit">
+      <article class="footer-adress">
+        <h2 class="footer-info-adress h2-title">Adres</h2>
+        <p class="footer-info-adress__details p-normal">ul. Słupecka 45 <span></span>81-313 Gdynia
+        </p>
+      </article>
+      <article class="footer-map">
+        <div class="footer-map__g-map">MAPA</div>
+      </article>
+    </section>
+    <div class="wrap-contact">
+      <section class="wrap-footer__section footer-contact">
+        <h2 class="wrap-footer__footer-info-contact h2-title">Kontakt</h2>
+        <p class="wrap-footer__footer-info-contact__details p-normal">tel: <a href="tel:+48123123123">123123123</a>
+        </p>
+        <p class="wrap-footer__footer-info-contact__details p-normal">e-mail: <a
+            href="mailto: skoraodnowagdynia@gmail.com">
+            skoraodnowagdynia@gmail.com</a>
+        </p>
+        <div class="wrap-footer__social-button">
+          <a href="" class="wrap-footer__social-button-icon wrap-footer__social-button-icon--fb"><img
+              src="<?php echo get_theme_file_uri('/img/facebook.png')?>" alt="Facebook Skóra odnowa Gdynia" /></a>
+          <a href="" class="wrap-footer__social-button-icon wrap-footer__social-button-icon--insta"><img
+              src="<?php echo get_theme_file_uri('/img/instagram.png')?>" alt="Instagram Skóra odnowa Gdynia" /></a>
+        </div>
+      </section>
+      <section class="wrap-footer__section wrap-btn-visit">
+        <button class="btn-visit"><a href="" class="visit-place ">Umów wizytę stacjonarną</a></button>
+        <button class="btn-visit btn-visit--second"><a href="" class="visit-online">Umów spotkanie online</a></button>
+      </section>
+      <section class="wrap-footer__section footer-open">
+        <h2 class="wrap-footer__footer-open-info h2-title">Godziny otwarcia</h2>
+        <p class="wrap-footer__footer-open-info__details p-normal">
+          Poniedziałek-piątek<br>
+          8:00 - 20:00</p>
+        <p class="wrap-footer__footer-open-info__details p-normal">Sobota<br>
+          8:00 - 15:00
+        </p>
+      </section>
+    </div>
+  </div>
+  <section class="wrap-footer__section wrap-footer__section-copy-reg">
+    <div class="wrap-footer__section-wrap-copy">
+      <div class="wrap-copy-reg">
+        <p class="p-normal p-normal__copy-reg"><a href="/">Regulamin</a>
+        </p>
+        <p class="p-normal p-normal__copy-reg"><a href="/">Polityka prywatności</a>
+        </p>
+      </div>
+      <p class="p-normal p-normal__copy-reg p-normal__copy-reg--rights">© Skóra odnowa All Rights Reserved</p>
+    </div>
+  </section>
 
+</footer>
 <?php wp_footer(); ?>
 </body>
 
--- app/public/wp-content/themes/skoraodnowatheme/functions.php
+++ app/public/wp-content/themes/skoraodnowatheme/functions.php
@@ -3,6 +3,7 @@
 function skora_odnowa(){
   wp_enqueue_style('skoraodnowa_main_styles', get_theme_file_uri('/styles/main.css'));
   wp_enqueue_style('skoraodnowa_nav_styles', get_theme_file_uri('/styles/navigation.css'));
+  wp_enqueue_style('skoraodnowa_footer_styles', get_theme_file_uri('/styles/footer.css'));
 }
 
 add_action('wp_enqueue_scripts', 'skora_odnowa')
